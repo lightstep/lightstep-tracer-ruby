d68a618642d284ca770612e3fe84a068aeda6d38
bcronin <bcronin@resonancelabs.com>
2016-04-18 16:34:10 -0700

    Create nil transport for unit tests. Add more unit tests.

---
+++ lib/lightstep/tracer/transports/transport_nil.rb
@@ -0,0 +1,12 @@
+# Empty transport, primarily for unit testing purposes
+class TransportNil
+  def initialize
+  end
+
+  def ensure_connection(options)
+  end
+
+  def flush_report(_auth, _report)
+    nil
+  end
+end

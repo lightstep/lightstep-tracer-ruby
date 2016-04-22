68e5e0ddf2bb2c48c3c041d7070c4e5d3b064ac9
bcronin <bcronin@resonancelabs.com>
2016-04-22 11:29:10 -0700

    Add threading unit tests

--- example.rb
+++ example.rb
@@ -1,10 +1,6 @@
 require_relative './lib/lightstep-tracer.rb'
 
-LightStep.init_global_tracer('lightstep/ruby/example', '{your_access_token}', {
-                               #:collector_host => 'localhost',
-                               #:collector_port => 9998,
-                               #:collector_encryption => 'none',
-                             })
+LightStep.init_global_tracer('lightstep/ruby/example', '{your_access_token}')
 
 puts 'Starting operation...'
 span = LightStep.start_span('my_span')

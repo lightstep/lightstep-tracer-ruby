2a2c70d0948c87c7f2cb45bacac12ecaa35edbae
Nick Gauthier <ngauthier@gmail.com>
2016-11-03 16:55:53 -0400

    channel-signal based fork-safe implementation of reporter

--- examples/fork_children/main.rb
+++ examples/fork_children/main.rb
@@ -1,6 +1,5 @@
 # A simple, manual test ensuring that tracer instances still report after a
-# Process.fork. Currently this requires the tracer instance to be explicitly
-# disabled before the fork and reenabled afterward.
+# Process.fork.
 
 require 'bundler/setup'
 require 'lightstep'
@@ -14,19 +13,13 @@ puts 'Starting...'
 (1..20).each do |k|
   puts "Explicit reset iteration #{k}..."
 
-  # NOTE: the tracer is disabled and reenabled on either side of the fork
-  LightStep.disable
   pid = Process.fork do
-    LightStep.enable
     10.times do
       span = LightStep.start_span("my_forked_span-#{Process.pid}")
       sleep(0.0025 * rand(k))
       span.finish
     end
   end
-
-  # Also renable the parent process' tracer
-  LightStep.enable
 
   10.times do
     span = LightStep.start_span("my_process_span-#{Process.pid}")
@@ -35,6 +28,8 @@ puts 'Starting...'
   end
 
   # Make sure redundant enable calls don't cause problems
+  # NOTE: disabling discards the buffer by default, so all spans
+  # get cleared here except the final toggle span
   10.times do
     LightStep.disable
     LightStep.enable
@@ -48,7 +43,7 @@ puts 'Starting...'
   end
 
   puts "Parent, pid #{Process.pid}, waiting on child pid #{pid}"
-  Process.wait
+  Process.wait(pid)
 end
 
 puts 'Done!'

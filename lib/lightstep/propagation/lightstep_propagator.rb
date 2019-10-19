ed151be6deda706e6fcdd19dc933d2c395acd063
Matthew Wear <mwear@lightstep.com>
2019-10-19 16:51:12 -0700

    Make LightStepPropagator easier to subclass
    Constant lookup will search in Module.nesting before looking up
    the inheritance chain, which makes constants hard to override
    when subclassing. This commit prefixes constants in the
    LightStep propagator with self.class in order to steer lookup
    to the inheritance chain.

--- lib/lightstep/propagation/lightstep_propagator.rb
+++ lib/lightstep/propagation/lightstep_propagator.rb
@@ -3,11 +3,11 @@
 module LightStep
   module Propagation
     class LightStepPropagator
-      CARRIER_TRACER_STATE_PREFIX = 'ot-tracer-'.freeze
-      CARRIER_BAGGAGE_PREFIX = 'ot-baggage-'.freeze
-      CARRIER_SPAN_ID = (CARRIER_TRACER_STATE_PREFIX + 'spanid').freeze
-      CARRIER_TRACE_ID = (CARRIER_TRACER_STATE_PREFIX + 'traceid').freeze
-      CARRIER_SAMPLED = (CARRIER_TRACER_STATE_PREFIX + 'sampled').freeze
+      CARRIER_TRACER_STATE_PREFIX = 'ot-tracer-'
+      CARRIER_SPAN_ID = 'ot-tracer-spanid'
+      CARRIER_TRACE_ID = 'ot-tracer-traceid'
+      CARRIER_SAMPLED = 'ot-tracer-sampled'
+      CARRIER_BAGGAGE_PREFIX = 'ot-baggage-'
 
       # Inject a SpanContext into the given carrier
       #
@@ -49,47 +49,48 @@ module LightStep
       private
 
       def inject_to_text_map(span_context, carrier)
-        carrier[CARRIER_SPAN_ID] = span_context.id
-        carrier[CARRIER_TRACE_ID] = span_context.trace_id unless span_context.trace_id.nil?
-        carrier[CARRIER_SAMPLED] = 'true'
+        carrier[self.class::CARRIER_SPAN_ID] = span_context.id
+        carrier[self.class::CARRIER_TRACE_ID] = span_context.trace_id unless span_context.trace_id.nil?
+        carrier[self.class::CARRIER_SAMPLED] = 'true'
 
         span_context.baggage.each do |key, value|
-          carrier[CARRIER_BAGGAGE_PREFIX + key] = value
+          carrier[self.class::CARRIER_BAGGAGE_PREFIX + key] = value
         end
       end
 
       def extract_from_text_map(carrier)
         # If the carrier does not have both the span_id and trace_id key
         # skip the processing and just return a normal span
-        if !carrier.has_key?(CARRIER_SPAN_ID) || !carrier.has_key?(CARRIER_TRACE_ID)
+        if !carrier.has_key?(self.class::CARRIER_SPAN_ID) || !carrier.has_key?(self.class::CARRIER_TRACE_ID)
           return nil
         end
 
         baggage = carrier.reduce({}) do |baggage, (key, value)|
-          if key.start_with?(CARRIER_BAGGAGE_PREFIX)
-            plain_key = key.to_s[CARRIER_BAGGAGE_PREFIX.length..key.to_s.length]
+          if key.start_with?(self.class::CARRIER_BAGGAGE_PREFIX)
+            plain_key = key.to_s[self.class::CARRIER_BAGGAGE_PREFIX.length..key.to_s.length]
             baggage[plain_key] = value
           end
           baggage
         end
+
         SpanContext.new(
-          id: carrier[CARRIER_SPAN_ID],
-          trace_id: carrier[CARRIER_TRACE_ID],
+          id: carrier[self.class::CARRIER_SPAN_ID],
+          trace_id: carrier[self.class::CARRIER_TRACE_ID],
           baggage: baggage,
         )
       end
 
       def inject_to_rack(span_context, carrier)
-        carrier[CARRIER_SPAN_ID] = span_context.id
-        carrier[CARRIER_TRACE_ID] = span_context.trace_id unless span_context.trace_id.nil?
-        carrier[CARRIER_SAMPLED] = 'true'
+        carrier[self.class::CARRIER_SPAN_ID] = span_context.id
+        carrier[self.class::CARRIER_TRACE_ID] = span_context.trace_id unless span_context.trace_id.nil?
+        carrier[self.class::CARRIER_SAMPLED] = 'true'
 
         span_context.baggage.each do |key, value|
           if key =~ /[^A-Za-z0-9\-_]/
             # TODO: log the error internally
             next
           end
-          carrier[CARRIER_BAGGAGE_PREFIX + key] = value
+          carrier[self.class::CARRIER_BAGGAGE_PREFIX + key] = value
         end
       end
 
@@ -97,7 +98,8 @@ module LightStep
         extract_from_text_map(env.reduce({}){|memo, (raw_header, value)|
           header = raw_header.to_s.gsub(/^HTTP_/, '').tr!('_', '-').downcase!
 
-          memo[header] = value if header.start_with?(CARRIER_TRACER_STATE_PREFIX, CARRIER_BAGGAGE_PREFIX)
+          memo[header] = value if header.start_with?(self.class::CARRIER_TRACER_STATE_PREFIX,
+                                                     self.class::CARRIER_BAGGAGE_PREFIX)
           memo
         })
       end
